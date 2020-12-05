f01d40bdd3f5af693bb02ba016d640d2261e72c5
Mohammad Ali Syed <alisyed840@hotmail.co.uk>
2020-12-05 17:06:29 +0000

    Added functionality to main page, added other pages

---
+++ App/app/app/src/main/java/com/example/fisev2concierge/HelpView.java
@@ -0,0 +1,17 @@
+package com.example.fisev2concierge;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpView extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_view);
+    }
+
+}
