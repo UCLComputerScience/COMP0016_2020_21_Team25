6202b2aa981a3a08e23c8e0f5330cbb676dbfb2d
Calin Hadarean <zcabch0@ucl.ac.uk>
2021-02-22 16:11:41 +0000

    Added AskBob Plugin Files

---
+++ AskBob Concierge/plugins/concierge_entertainment/actions.py
@@ -0,0 +1,101 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import askbob.plugin
+import requests
+
+@askbob.plugin.action("concierge_entertainment", "fetch_joke")
+class ActionConciergeFetchJoke(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        joke_term = next(tracker.get_latest_entity_values("joke_category"), None)
+        print(joke_term)
+        r = requests.get(url="http://0.0.0.0:8080/joke", params={
+            "TERM":joke_term}).json()
+        
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "joke",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+        return []
+
+
+@askbob.plugin.action("concierge_entertainment", "fetch_news")
+class ActionConciergeFetchNews(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        search_term = next(tracker.get_latest_entity_values("news_search_term"), None)
+        print(search_term)
+        r = requests.get(url="http://0.0.0.0:8080/news", params={
+            "QUERY": search_term,
+            "LANGUAGE": "en"
+        }).json()
+        
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "news",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+        return []
+
+@askbob.plugin.action("concierge_entertainment", "fetch_book")
+class ActionConciergeFetchBook(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        search_term = next(tracker.get_latest_entity_values("book_search_term"), None)
+        print(search_term)
+        r = requests.get(url="http://0.0.0.0:8080/book", params={
+            "QUERY": search_term,
+            "LANGUAGES": "en"
+        }).json()
+        
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "book",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+        return []
+
+@askbob.plugin.action("concierge_entertainment", "fetch_book_category")
+class ActionConciergeFetchBookCategory(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        category = next(tracker.get_latest_entity_values("book_category"), None)
+        print(category)
+        r = requests.get(url="http://0.0.0.0:8080/book", params={
+            "TOPIC": category,
+            "LANGUAGES": "en"
+        }).json()
+        
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "book",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+        return []
---
+++ AskBob Concierge/plugins/concierge_finance/actions.py
@@ -0,0 +1,87 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import askbob.plugin
+import requests
+
+
+@askbob.plugin.action("concierge_finance", "fetch_stocks")
+class ActionConciergeFetchStocks(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        
+        stock= next(tracker.get_latest_entity_values("ORG"), None)
+        print(stock)
+        r = requests.get(url="http://0.0.0.0:8080/stocks", params={
+            "SYMBOL": stock,
+        }).json()
+        
+
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "stocks",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+
+        return []
+
+
+@askbob.plugin.action("concierge_finance", "fetch_charity")
+class ActionConciergeFetchCharity(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        search_term = next(tracker.get_latest_entity_values("charity_search_term"), None)
+
+
+        r = requests.get(url="http://0.0.0.0:8080/charity-search", params={
+            "QUERY": search_term,
+            
+        }).json()
+        
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "charity-search",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+
+        return []
+
+@askbob.plugin.action("concierge_finance", "fetch_charity_city")
+class ActionConciergeFetchCharityCity(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = next(tracker.get_latest_entity_values("GPE"), None)
+
+
+        r = requests.get(url="http://0.0.0.0:8080/charity-by-city", params={
+            "CITY": city,
+            
+        }).json()
+        
+        data_package={
+            "Service_Type": "API_CALL",
+            "Service": "charity-by-city",
+            "Response": r["message"]
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+
+        return []
+
