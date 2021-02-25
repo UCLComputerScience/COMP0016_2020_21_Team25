dbaefe2c54476f887f71cffd1da473a1b17a5b53
Calin Hadarean <zcabch0@ucl.ac.uk>
2021-02-25 17:59:06 +0000

    Added support to open applications to NLP

---
+++ AskBob Concierge/plugins/concierge_apps/actions.py
@@ -0,0 +1,31 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import askbob.plugin
+import requests
+
+
+@askbob.plugin.action("concierge_apps", "open_app")
+class ActionConciergePlaceCall(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        application = next(tracker.get_latest_entity_values("application"), None)
+        response=application
+        if not application:
+            response="Sorry, I can't open this app"
+       
+        data_package={
+            "Service_Type": "OPEN_APP",
+            "Application": response
+        }
+
+        dispatcher.utter_message(json_message= data_package)
+
+
+        return []
+
+
